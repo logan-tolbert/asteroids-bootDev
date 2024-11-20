14c97fddc233a11419ddde49b80f89e16436a8fa
logan-tolbert <lodev205@gmail.com>
2024-11-20 11:06:45 -0600

    :recycle: Refactor main loop to use sprite groups for drawing and updating

--- main.py
+++ main.py
@@ -3,22 +3,29 @@ from constants import *
 from player import *
 
 def main():
+    
     pygame.init()
     clock = pygame.time.Clock()
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     dt = clock.tick(60) / 1000
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+       
+        for obj in updatable:
+            obj.update(dt)  
+
         pygame.display.flip()
-              
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+        
 
 if __name__ == "__main__":
     main()
