581e2e11f8100e6d3eb051d378a174d1931e2836
logan-tolbert <lodev205@gmail.com>
2024-11-23 17:01:58 -0600

    :recycle: Refactor main.py

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
 import pygame
 from constants import *
-from player import *
-from asteroid import *
+from player import Player
+
 
 def main():
-    
     pygame.init()
-    clock = pygame.time.Clock()
-    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-    dt = clock.tick(60) / 1000
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updatable = pygame.sprite.Group(player)
-    drawable = pygame.sprite.Group(player)
-    asteroids = pygame.sprite.Group(Asteroid)
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
-    Asteroid.containers = (asteroids, drawable, updatable)
-    
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
-        
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
         for obj in drawable:
             obj.draw(screen)
-       
-        for obj in updatable:
-            obj.update(dt)  
 
         pygame.display.flip()
-        
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
