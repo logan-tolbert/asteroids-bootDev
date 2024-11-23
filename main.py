363c5dc551db35e233ea6c8c151cd929fe09f847
logan-tolbert <lodev205@gmail.com>
2024-11-23 17:13:00 -0600

    :construction: Integrate AsteroidField into main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
-
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,9 +15,11 @@ def main():
     
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+    asteroid_field = AsteroidField()
+    
     dt = 0
 
     while True:
